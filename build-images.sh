35864b8fb1e0b008cbc800b73b950aa7506b2082
Adam Hartley <BytesGuy@users.noreply.github.com>
2021-11-30 17:28:01 +0000

    Publish v8.0.26 and 5.7.35 [release]

---
+++ build-images.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+docker build --file 8.0/Dockerfile -t cimg/mysql:8.0.27  -t cimg/mysql:8.0 .
+docker build --file 5.7/Dockerfile -t cimg/mysql:5.7.36  -t cimg/mysql:5.7 .
---
+++ push-images.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+docker push cimg/mysql:8.0.27
+docker push cimg/mysql:8.0
+
+docker push cimg/mysql:5.7.36
+docker push cimg/mysql:5.7
