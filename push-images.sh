a2fe4945300b870a9cecf888509cf0002c4aabfd
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-08-18 23:34:59 -0400

    Publish v5.7.37. [release]

--- push-images.sh
+++ push-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker push cimg/mysql:8.0.28
-docker push cimg/mysql:8.0
+docker push cimg/mysql:5.7.37
+docker push cimg/mysql:5.7
