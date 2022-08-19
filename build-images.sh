70b8651013dc19c525e04d2adfa243c0ade33a24
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-08-19 15:36:43 -0400

    Publish v5.7.38. [release] (#15)

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 5.7/Dockerfile -t cimg/mysql:5.7.37  -t cimg/mysql:5.7 .
+docker build --file 5.7/Dockerfile -t cimg/mysql:5.7.38  -t cimg/mysql:5.7 .
