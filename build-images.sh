54c056f854f4605e9d5ec07fa9f02f31469bea37
Justin Collier <jc9000@cisco.com>
2025-03-18 21:49:12 -0600

    8.0.32 support continuation

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 8.0/Dockerfile -t cimg/mysql:8.0.33  -t cimg/mysql:8.0 .
+docker build --file 8.0/Dockerfile -t cimg/mysql:8.0.33 -t cimg/mysql:8.0 .
+
+docker build --file 8.0.32/Dockerfile -t cimg/mysql:8.0.32 .
