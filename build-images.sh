bc2d61cbe87c52e2a14d331af51743972ef1aac7
Joe Cai <joe.cai@bigcommerce.com>
2025-04-22 22:56:17 +1000

    Build cimg/mysql:8.0.42

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 8.0/Dockerfile -t cimg/mysql:8.0.33  -t cimg/mysql:8.0 .
+docker build --file 8.0/Dockerfile -t cimg/mysql:8.0.42  -t cimg/mysql:8.0 .
